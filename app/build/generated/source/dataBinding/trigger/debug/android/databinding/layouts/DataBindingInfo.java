90b877c5d7c217d90f47388e375d6df5726675ee
Vineet Jain <vineet.jain@mpowersoft.com>
2019-01-18 10:58:08 +0530

    removed build folder

--- app/build/generated/source/dataBinding/trigger/debug/android/databinding/layouts/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package android.databinding.layouts;
-
-import android.databinding.BindingBuildInfo;
-
-@BindingBuildInfo(buildId="92520a3c-a4f1-4ed4-a799-ae88f8b4285b")
-public class DataBindingInfo {}
